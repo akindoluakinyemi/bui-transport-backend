a01465526ba03a46683cf0d884f47415ab9efce5
akinyemi akindolu <akinyemiakindolu2@gmail.com>
2025-06-02 15:24:01 +0100

    Add student for postman testing

--- bui_transport/settings.py
+++ bui_transport/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
